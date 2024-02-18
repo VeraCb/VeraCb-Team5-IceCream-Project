496a3d219154f7a1d0aed77ce157dd687bcf8992
VeraCb <149841733+VeraCb@users.noreply.github.com>
2024-02-18 22:43:59 +0000

    Luca's sections

---
+++ js/mode.js
@@ -0,0 +1,29 @@
+// Luca's sections
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-openx]"),
+    closeModalBtn: document.querySelector("[data-modal-closex]"),
+    modal: document.querySelector("[data-modalx]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
